3b5505ad935d69f6173c3e0ad31f6ba627ee46ef
papergrapefruit <statofeld@gmail.com>
2017-09-23 10:00:54 -0400

    adding stuff

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,17 @@
 var express = require("express");
 
 var router = express.Router();
+var passport = require('passport');
 
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+var env = {
+  AUTH0_CLIENT_ID: 'clSCUHBZd0T-P3Yg8M5b1je6L30lU8S0',
+  AUTH0_DOMAIN: 'app76371509.auth0.com',
+  AUTH0_CALLBACK_URL: 'https://enigmatic-caverns-14972.herokuapp.com/callback'
+};
+
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
@@ -37,6 +45,42 @@ router.put("/:id", function (req, res) {
     res.redirect("/");
   });
 });
+
+
+
+
+// Perform the login
+router.get(
+  '/login',
+  passport.authenticate('auth0', {
+    clientID: env.AUTH0_CLIENT_ID,
+    domain: env.AUTH0_DOMAIN,
+    redirectUri: env.AUTH0_CALLBACK_URL,
+    audience: 'https://' + env.AUTH0_DOMAIN + '/userinfo',
+    responseType: 'code',
+    scope: 'openid'
+  }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+// Perform session logout and redirect to homepage
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// Perform the final stage of authentication and redirect to '/user'
+router.get(
+  '/callback',
+  passport.authenticate('auth0', {
+    failureRedirect: '/'
+  }),
+  function (req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  }
+);
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
 var port = process.env.PORT || 3006;
 
@@ -24,5 +26,35 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
+
+
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: 'app76371509.auth0.com',
+    clientID: 'clSCUHBZd0T-P3Yg8M5b1je6L30lU8S0',
+    clientSecret: '5EmkLn5n7w2b_YeLuZYjStnu_WqEEsHq6drAMGA5V8huTjVBi9oiC5BIBrPvSJmQ',
+    callbackURL: 'https://enigmatic-caverns-14972.herokuapp.com/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+// ...
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port);
